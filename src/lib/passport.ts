52b1de5550ba5c81041c07589c177ae987860984
Shannon Lal <shannonlal@hotmail.com>
2025-02-15 10:09:50 -0500

    Check in latest version

--- src/lib/passport.ts
+++ src/lib/passport.ts
@@ -1,50 +1,58 @@
 import passport from 'passport';
 import { Strategy as LocalStrategy } from 'passport-local';
+import { User } from '../models/User';
 import bcrypt from 'bcryptjs';
-import { User, IUserDocument } from '../models/User';
+import { IUserDocument } from '../models/User';
 
+// Configure Local Strategy
 passport.use(
   new LocalStrategy(
     {
       usernameField: 'email',
       passwordField: 'password',
     },
-    async (email, password, done) => {
+    async (
+      email: string,
+      password: string,
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      done: (error: any, user?: any, options?: { message: string }) => void
+    ) => {
       try {
         const user = await User.findOne({ email });
-
         if (!user) {
           return done(null, false, { message: 'Invalid email or password' });
         }
-
         const isMatch = await bcrypt.compare(password, user.password);
-
         if (!isMatch) {
           return done(null, false, { message: 'Invalid email or password' });
         }
-
         return done(null, user);
-      } catch (error) {
-        return done(error);
+      } catch (err) {
+        return done(err);
       }
     }
   )
 );
 
-passport.serializeUser((user: IUserDocument, done) => {
-  done(null, user._id);
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+passport.serializeUser((user: IUserDocument, done: (err: any, id?: string) => void) => {
+  done(null, user._id.toString());
 });
 
-passport.deserializeUser(async (id: string, done) => {
-  try {
-    const user = await User.findById(id);
-    if (!user) {
-      return done(new Error('User not found'));
+passport.deserializeUser(
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  async (id: string, done: (err: any, user?: IUserDocument | false) => void) => {
+    try {
+      const user = await User.findById(id);
+      if (!user) {
+        return done(new Error('User not found'));
+      }
+      done(null, user);
+    } catch (err) {
+      done(err);
     }
-    done(null, user);
-  } catch (error) {
-    done(error);
   }
-});
+);
 
 export default passport;
